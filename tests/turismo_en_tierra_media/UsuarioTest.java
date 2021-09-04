fe158498581d352ba4ebb5d4e40710e8b0c04d9e
Andres Pierandrei <andres.pierandrei@gmail.com>
2021-09-03 22:29:24 -0300

    Ahora los Usuarios son leidos desde Archivo, Agregué una prueba para que verifique que se leyó bien TipoDeAtracción

--- tests/turismo_en_tierra_media/UsuarioTest.java
+++ tests/turismo_en_tierra_media/UsuarioTest.java
@@ -12,37 +12,45 @@ import org.junit.Test;
 public class UsuarioTest {
 
 
-	Usuario[] personas = new Usuario[4];
+	//Usuario[] personas = new Usuario[4];
+	List<Usuario> personas = new ArrayList<Usuario>();
+	
 	@Before
 	public void setup() {
-		personas[0] = new Usuario("Eowyn", TipoDeAtraccion.AVENTURA, 10, 8);
-		personas[1] = new Usuario("Gandalf", TipoDeAtraccion.PAISAJE, 100, 5);
-		personas[2] = new Usuario("Sam", TipoDeAtraccion.DEGUSTACION, 36, 8);
-		personas[3] = new Usuario("Galadriel", TipoDeAtraccion.PAISAJE, 120, 1);
+		lectorDeArchivos lector = new lectorDeArchivos();
+		lector.leerUsuarios(personas);
 	}
 	
 	@Test
 	public void noAceptaValoresNegativos(){
 		Usuario persona= new Usuario("Marina",TipoDeAtraccion.AVENTURA, 100,-5);
-		assertEquals(0, persona.getTiempoDisponible());
+		assertEquals(0, persona.getTiempoDisponible(),0); //le fataba el parámetro de delta para comparar doubles.
+	}
+	
+	@Test
+	public void pruebaDeLecturadeTipoDeAtraccion() {
+		assertEquals(TipoDeAtraccion.AVENTURA, personas.get(0).getAtraccionFavorita());
+		assertEquals(TipoDeAtraccion.DEGUSTACION, personas.get(1).getAtraccionFavorita());
+		assertEquals(TipoDeAtraccion.PAISAJE, personas.get(2).getAtraccionFavorita());
+		assertEquals(TipoDeAtraccion.PAISAJE, personas.get(3).getAtraccionFavorita());
 	}
 	
 	@Test
 	public void usuarioToString() {
-		assertEquals("Eowyn tiene 10 monedas, 8 horas libres y su tipo de atraccion favorito es Aventura", personas[0].toString());
+		assertEquals("Eowyn tiene 10 monedas, 8.0 horas libres y su tipo de atraccion favorito es Aventura", personas.get(0).toString());
 	}
 	
 	@Test
 	public void agregarUnaAtraccionAlItinerario() {
 		Atraccion atraccion1= new Atraccion("Moria", 10, 2, 6, TipoDeAtraccion.AVENTURA);
 		
-		atraccion1.reservarLugar(personas[0]);
+		atraccion1.reservarLugar(personas.get(0));
 		
 		assertEquals(5,atraccion1.getUsosDisponibles());
-		assertEquals(6,personas[0].getTiempoDisponible());
+		assertEquals(6,personas.get(0).getTiempoDisponible(),0); //también le faltaba el tercer parámetro.
 		List<Atraccion> atraccionEquals= new ArrayList<Atraccion>();
 		atraccionEquals.add(atraccion1); 
-		assertEquals(atraccionEquals, personas[0].getItinerario());
+		assertEquals(atraccionEquals, personas.get(0).getItinerario());
 	}
 	
 
