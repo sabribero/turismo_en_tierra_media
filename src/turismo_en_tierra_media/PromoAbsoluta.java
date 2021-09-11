36142a729a5b5c7b0289b1a8146ac660eb787a0b
Andres Pierandrei <andres.pierandrei@gmail.com>
2021-09-11 06:21:46 -0300

    agregue variables que sepan que tipo de atraccion y promo es.

--- src/turismo_en_tierra_media/PromoAbsoluta.java
+++ src/turismo_en_tierra_media/PromoAbsoluta.java
@@ -2,28 +2,23 @@ package turismo_en_tierra_media;
 
 import java.util.List;
 
-public class PromoAbsoluta extends Promocion{
-
+public class PromoAbsoluta extends Promocion {
 
 /*
- * 
- * Toma como valor de la promocion al indicado 
- * 
- * con valorPromo
+ * Toma como valor de la promocion al indicado con valorPromo 
  */
 	
 	public PromoAbsoluta(List<Atraccion> atraccionesEnPromo, int valorPromo) {
 		
 		super(atraccionesEnPromo, valorPromo);
 		this.atraccionesEnPromocion=atraccionesEnPromo;
-		this.valorPromo= valorPromo;
+		
+		this.tipoAtraccion = atraccionesEnPromo.get(0).getTipo(); // asumo que habrá almenos una atracción y todas en la lista serán del mismo tipo
+		this.tipo = TipoDePromo.ABSOLUTA;
 		
 		this.tiempoPromo=0;
 		for(Atraccion cadaUna : atraccionesEnPromo) {
-			
-			this.tiempoPromo=+cadaUna.getTiempoDeUso();
+			this.tiempoPromo += cadaUna.getTiempoDeUso(); //decía "=+", para que acumule tiene que ser "+="
 			}
-	}
-	
-	
+	}	
 }
