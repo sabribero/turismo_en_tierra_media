3e40ea2767cbc4fc9dc30e5713d3f531cee323c3
Andres Pierandrei <andres.pierandrei@gmail.com>
2021-09-11 06:26:02 -0300

    agregue variables que sepan que tipo de atraccion y promo es.

--- src/turismo_en_tierra_media/PromoPorcentual.java
+++ src/turismo_en_tierra_media/PromoPorcentual.java
@@ -2,34 +2,26 @@ package turismo_en_tierra_media;
 
 import java.util.List;
 
-public class PromoPorcentual extends Promocion{
-	
+public class PromoPorcentual extends Promocion {
 
 	/*
-	 * 
-	 * Descuenta el porcentaje de valor promo del valor total (suma) de todas las atracciones
-	 * incluidas en la promo.
+	 * Descuenta el porcentaje de valor promo del valor total (suma) de todas las atracciones incluidas en la promo. 
 	 */
 
-	public PromoPorcentual(List<Atraccion> atraccionesEnPromo, int valorPromo) {
+	public PromoPorcentual(List<Atraccion> atraccionesEnPromo, int valorDescuento) {
 		
-		super(atraccionesEnPromo, valorPromo);
+		super(atraccionesEnPromo, valorDescuento);
 		this.atraccionesEnPromocion=atraccionesEnPromo;
 		
+		this.tipoAtraccion = atraccionesEnPromo.get(0).getTipo(); // asumo que habrá almenos una atracción y todas en la lista serán del mismo tipo
+		this.tipo = TipoDePromo.PORCENTUAL;
+		
 		this.valorPromo=0;
-		for(Atraccion cadaUna: atraccionesEnPromo) {
-			
-			this.valorPromo=+cadaUna.getValor();
-		}
-		
-		
+		for(Atraccion cadaUna: atraccionesEnPromo)
+			this.valorPromo += Math.round(cadaUna.getValor()*(1-(valorDescuento/100.0)));	// sumatoria de los valores de cada atracción de la promoción con descuento
+																							// Hago un redondeo para obtener un número entero de monedas
 		this.tiempoPromo=0;
-		for(Atraccion cadaUna : atraccionesEnPromo) {
-			
-			this.tiempoPromo=+cadaUna.getTiempoDeUso();
-			}
-		
-	}
-
-
+		for(Atraccion cadaUna : atraccionesEnPromo)
+			this.tiempoPromo+=cadaUna.getTiempoDeUso();
+	}	
 }
